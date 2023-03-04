172551e54f754c099ebc189026df1b35982078a6
aadi-thedeyguy <adikhare24@gmail.com>
2023-03-04 16:09:33 +0530

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var http = require('http')
+var http = require("http");
 
-http.createServer(function (req, res) {
-  res.end();
-}).listen(4000, () => {
-    console.log(`Aditya`)
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  switch (url) {
+    case "/":
+        res.write("Welcome to Main App Page");
+        return res.end();
+        
+    case "/home":
+      res.write("Welcome home");
+      return res.end();
+
+    case "/about":
+      res.write("Welcome to About Us page");
+      return res.end();
+
+    case "/node":
+      res.write("Welcome to my Node Js project");
+      return res.end();
+
+  }
 });
+
+server.listen(3000);
