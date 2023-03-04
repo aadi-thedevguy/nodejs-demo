b6a694acfd34f7aa8c395c6d62fc92a79f2cd452
aadi-thedeyguy <adikhare24@gmail.com>
2023-03-04 17:50:27 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-var http = require("http");
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  switch (url) {
-    case "/":
-        res.write("Welcome to Main App Page");
-        return res.end();
-        
-    case "/home":
-      res.write("Welcome home");
-      return res.end();
-
-    case "/about":
-      res.write("Welcome to About Us page");
-      return res.end();
-
-    case "/node":
-      res.write("Welcome to my Node Js project");
-      return res.end();
-
+  const method = req.method;
+  if (url === '/') {
+    let data = fs.readFileSync('message.txt',{encoding: 'utf-8'})
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write(`<body><h3>${data || 'Your Message'} </h3><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`);
+    res.write('</html>');
+    return res.end();
   }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+    //   console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
